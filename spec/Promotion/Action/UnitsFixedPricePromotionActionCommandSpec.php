5aa8326001cf2bc06d1b6da2276fa62ff0ac3eb9
Roshyo <stefdecock68@gmail.com>
2020-06-16 11:18:22 +0200

    Adding specific tests for described use-cases

--- spec/Promotion/Action/UnitsFixedPricePromotionActionCommandSpec.php
+++ spec/Promotion/Action/UnitsFixedPricePromotionActionCommandSpec.php
@@ -7,6 +7,7 @@ namespace spec\Setono\SyliusPromotionExtensionsPlugin\Promotion\Action;
 use Doctrine\Common\Collections\ArrayCollection;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Setono\SyliusPromotionExtensionsPlugin\Distributor\MostExpensiveFirstDistributor;
 use Setono\SyliusPromotionExtensionsPlugin\Distributor\MostExpensiveFirstDistributorInterface;
 use Setono\SyliusPromotionExtensionsPlugin\Promotion\Action\UnitsFixedPricePromotionActionCommand;
 use Sylius\Component\Core\Model\ChannelInterface;
@@ -30,6 +31,7 @@ final class UnitsFixedPricePromotionActionCommandSpec extends ObjectBehavior
         MostExpensiveFirstDistributorInterface $distributor
     ): void {
         $this->beConstructedWith($adjustmentFactory, $priceRangeFilter, $taxonFilter, $productFilter, $distributor);
+        $adjustmentFactory->createNew()->willReturn(new Adjustment());
     }
 
     public function it_is_initializable(): void
@@ -120,7 +122,6 @@ final class UnitsFixedPricePromotionActionCommandSpec extends ObjectBehavior
         OrderInterface $subject,
         PromotionInterface $promotion,
         ChannelInterface $channel,
-        FactoryInterface $adjustmentFactory,
         FilterInterface $priceRangeFilter,
         FilterInterface $taxonFilter,
         FilterInterface $productFilter,
@@ -129,35 +130,148 @@ final class UnitsFixedPricePromotionActionCommandSpec extends ObjectBehavior
         $channel->getCode()->willReturn('channel_code');
         $subject->getChannel()->willReturn($channel);
 
-        $item1 = new OrderItem();
-        $item1->setUnitPrice(60);
-        $unit11 = new OrderItemUnit($item1);
+        $item1 = $this->getItem(60);
         $unit12 = new OrderItemUnit($item1);
-        $item1->addUnit($unit11);
         $item1->addUnit($unit12);
-        $item2 = new OrderItem();
-        $unit21 = new OrderItemUnit($item2);
-        $item2->addUnit($unit21);
-        $item2->setUnitPrice(60);
+        $item2 = $this->getItem(60);
 
         $subject->getItems()->willReturn(new ArrayCollection([$item1, $item2]));
         $priceRangeFilter->filter([$item1, $item2], Argument::any())->willReturn([$item1, $item2]);
         $taxonFilter->filter([$item1, $item2], Argument::any())->willReturn([$item1, $item2]);
         $productFilter->filter([$item1, $item2], Argument::any())->willReturn([$item1, $item2]);
 
-        $distributor->distribute([60, 60, 60], 30)->willReturn([10, 10, 10]);
-        $adjustmentFactory->createNew()->willReturn(new Adjustment());
+        $realDistributor = new MostExpensiveFirstDistributor();
+        $distribution = $realDistributor->distribute([60, 60, 60], 30);
+        $distributor->distribute([60, 60, 60], 30)->willReturn($distribution);
 
         $this->execute($subject, $this->getValidConfiguration('channel_code'), $promotion)->shouldReturn(true);
     }
 
-    private function getValidConfiguration(string $channelCode): array
-    {
+    public function it_tests_specific_configuration_1(
+        OrderInterface $subject,
+        PromotionInterface $promotion,
+        ChannelInterface $channel,
+        FilterInterface $priceRangeFilter,
+        FilterInterface $taxonFilter,
+        FilterInterface $productFilter,
+        MostExpensiveFirstDistributorInterface $distributor
+    ): void {
+        $channel->getCode()->willReturn('channel_code');
+        $subject->getChannel()->willReturn($channel);
+
+        $item1 = $this->getItem(81);
+        $item2 = $this->getItem(30);
+        $item3 = $this->getItem(55);
+
+        $subject->getItems()->willReturn(new ArrayCollection([$item1, $item2, $item3]));
+        $priceRangeFilter->filter([$item1, $item2, $item3], Argument::any())->willReturn([$item1, $item2, $item3]);
+        $taxonFilter->filter([$item1, $item2, $item3], Argument::any())->willReturn([$item1, $item2, $item3]);
+        $productFilter->filter([$item1, $item2, $item3], Argument::any())->willReturn([$item1, $item2, $item3]);
+
+        $realDistributor = new MostExpensiveFirstDistributor();
+        $distribution = $realDistributor->distribute([81, 55, 30], 16);
+        $distributor->distribute([81, 55, 30], 16)->willReturn($distribution);
+
+        $this->execute($subject, $this->getValidConfiguration('channel_code', 150, 3), $promotion)->shouldReturn(true);
+    }
+
+    public function it_tests_specific_configuration_2(
+        OrderInterface $subject,
+        PromotionInterface $promotion,
+        ChannelInterface $channel,
+        FilterInterface $priceRangeFilter,
+        FilterInterface $taxonFilter,
+        FilterInterface $productFilter
+    ): void {
+        $channel->getCode()->willReturn('channel_code');
+        $subject->getChannel()->willReturn($channel);
+
+        $item1 = $this->getItem(105);
+        $item2 = $this->getItem(80);
+
+        $subject->getItems()->willReturn(new ArrayCollection([$item1, $item2]));
+        $priceRangeFilter->filter([$item1, $item2], Argument::any())->willReturn([$item1, $item2]);
+        $taxonFilter->filter([$item1, $item2], Argument::any())->willReturn([$item1, $item2]);
+        $productFilter->filter([$item1, $item2], Argument::any())->willReturn([$item1, $item2]);
+
+        $this->execute($subject, $this->getValidConfiguration('channel_code', 200, 2), $promotion)->shouldReturn(false);
+    }
+
+    public function it_tests_specific_configuration_3(
+        OrderInterface $subject,
+        PromotionInterface $promotion,
+        ChannelInterface $channel,
+        FilterInterface $priceRangeFilter,
+        FilterInterface $taxonFilter,
+        FilterInterface $productFilter,
+        MostExpensiveFirstDistributorInterface $distributor
+    ): void {
+        $channel->getCode()->willReturn('channel_code');
+        $subject->getChannel()->willReturn($channel);
+
+        $item1 = $this->getItem(105);
+        $item2 = $this->getItem(100);
+
+        $subject->getItems()->willReturn(new ArrayCollection([$item1, $item2]));
+        $priceRangeFilter->filter([$item1, $item2], Argument::any())->willReturn([$item1, $item2]);
+        $taxonFilter->filter([$item1, $item2], Argument::any())->willReturn([$item1, $item2]);
+        $productFilter->filter([$item1, $item2], Argument::any())->willReturn([$item1, $item2]);
+
+        $realDistributor = new MostExpensiveFirstDistributor();
+        $distribution = $realDistributor->distribute([105, 100], 5);
+        $distributor->distribute([105, 100], 5)->willReturn($distribution);
+
+        $this->execute($subject, $this->getValidConfiguration('channel_code', 200, 2), $promotion)->shouldReturn(true);
+    }
+
+    public function it_tests_specific_configuration_4(
+        OrderInterface $subject,
+        PromotionInterface $promotion,
+        ChannelInterface $channel,
+        FilterInterface $priceRangeFilter,
+        FilterInterface $taxonFilter,
+        FilterInterface $productFilter,
+        MostExpensiveFirstDistributorInterface $distributor
+    ): void {
+        $channel->getCode()->willReturn('channel_code');
+        $subject->getChannel()->willReturn($channel);
+
+        $item1 = $this->getItem(105);
+        $item2 = $this->getItem(100);
+        $item3 = $this->getItem(120);
+
+        $subject->getItems()->willReturn(new ArrayCollection([$item1, $item2, $item3]));
+        $priceRangeFilter->filter([$item1, $item2, $item3], Argument::any())->willReturn([$item1, $item2, $item3]);
+        $taxonFilter->filter([$item1, $item2, $item3], Argument::any())->willReturn([$item1, $item2, $item3]);
+        $productFilter->filter([$item1, $item2, $item3], Argument::any())->willReturn([$item1, $item2, $item3]);
+
+        $realDistributor = new MostExpensiveFirstDistributor();
+        $distribution = $realDistributor->distribute([120, 105, 100], 25);
+        $distributor->distribute([120, 105, 100], 25)->willReturn($distribution);
+
+        $this->execute($subject, $this->getValidConfiguration('channel_code', 200, 2), $promotion)->shouldReturn(true);
+    }
+
+    private function getValidConfiguration(
+        string $channelCode,
+        int $amount = null,
+        int $itemsAmount = null
+    ): array {
         return [
             $channelCode => [
-                'amount' => 100,
-                'itemsAmount' => 2,
+                'amount' => $amount ?? 100,
+                'itemsAmount' => $itemsAmount ?? 2,
             ],
         ];
+    }
+
+    private function getItem(int $amount): OrderItem
+    {
+        $item = new OrderItem();
+        $item->setUnitPrice($amount);
+        $unit = new OrderItemUnit($item);
+        $item->addUnit($unit);
+
+        return $item;
     }
 }
